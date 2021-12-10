62f451fef7f6e08b447745e7b784196a7cf91696
ocealot <kaminsky.michael@outlook.com>
2021-12-10 10:58:31 -0500

    set up SGE + RPR, still need to fill in

---
+++ JobBars/Jobs/RPR.cs
@@ -0,0 +1,36 @@
+﻿using JobBars.Buffs;
+using JobBars.Cooldowns;
+using JobBars.Cursors;
+using JobBars.Data;
+
+using JobBars.Gauges;
+using JobBars.Gauges.Charges;
+using JobBars.Gauges.GCD;
+using JobBars.Helper;
+using JobBars.Icons;
+using JobBars.UI;
+using System;
+
+namespace JobBars.Jobs {
+    public static class RPR {
+        public static GaugeConfig[] Gauges => new GaugeConfig[] {
+        };
+
+        public static BuffConfig[] Buffs => new BuffConfig[] {
+        };
+
+        public static Cursor Cursors => new(JobIds.RPR, CursorType.None, CursorType.GCD);
+
+        public static CooldownConfig[] Cooldowns => new CooldownConfig[] {
+        };
+
+        public static IconReplacer[] Icons => new IconReplacer[] {
+        };
+
+        public static bool MP => false;
+
+        public static float[] MP_SEGMENTS => null;
+
+        public static bool GCD_ROLL => true;
+    }
+}
---
+++ JobBars/Jobs/SGE.cs
@@ -0,0 +1,36 @@
+﻿using JobBars.Buffs;
+using JobBars.Cooldowns;
+using JobBars.Cursors;
+using JobBars.Data;
+
+using JobBars.Gauges;
+using JobBars.Gauges.Procs;
+using JobBars.Gauges.Timer;
+using JobBars.Helper;
+using JobBars.Icons;
+using JobBars.UI;
+using System;
+
+namespace JobBars.Jobs {
+    public static class SGE {
+        public static GaugeConfig[] Gauges => new GaugeConfig[] {
+        };
+
+        public static BuffConfig[] Buffs => new BuffConfig[] {
+        };
+
+        public static Cursor Cursors => new(JobIds.SGE, CursorType.None, CursorType.CastTime);
+
+        public static CooldownConfig[] Cooldowns => new CooldownConfig[] {
+        };
+
+        public static IconReplacer[] Icons => new IconReplacer[] {
+        };
+
+        public static bool MP => true;
+
+        public static float[] MP_SEGMENTS => new[] { 0.24f };
+
+        public static bool GCD_ROLL => false;
+    }
+}
