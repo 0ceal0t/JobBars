abd5b04ec23737aa759ca11494ab7a27e39d9fac
ocealot <kaminsky.michael@outlook.com>
2024-06-27 15:38:29 -0400

    Update Constants.cs

--- JobBars/Helper/Constants.cs
+++ JobBars/Helper/Constants.cs
@@ -1,8 +1,8 @@
 namespace JobBars.Helper {
     public static class Constants {
-        public static readonly string ReceiveActionEffectSig = "40 55 53 57 41 54 41 55 41 56 41 57 48 8D AC 24 ?? ?? ?? ?? 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 70";
+        public static readonly string ReceiveActionEffectSig = "40 55 56 57 41 54 41 55 41 56 48 8D AC 24 ?? ?? ?? ??";
         public static readonly string ActorControlSig = "E8 ?? ?? ?? ?? 0F B7 0B 83 E9 64";
-        public static readonly string IconDimmedSig = "E8 ?? ?? ?? ?? 49 8D 4D 10 FF C6";
+        public static readonly string IconDimmedSig = "E8 ?? ?? ?? ?? EB 04 4C 89 73 20";
 
         public static readonly string PlaySoundSig = "E8 ?? ?? ?? ?? 4D 39 BE ?? ?? ?? ??";
 
